6032e9d40d69efdf1688a0297806bcdd35057449
xxx222 <1390357620@qq.com>
2016-12-05 09:57:22 +0800

    aa
    aaa

--- src/com/example/xxx/MainActivity.java
+++ src/com/example/xxx/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-        zzz
+        zzzxxxxzxcsdfdfdf
     }
     
 }
